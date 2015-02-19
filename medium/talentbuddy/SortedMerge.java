f185aa415950bcf68527e09062b99cf00912f9b6
Christian Rubiales <christianrubiales@yahoo.com>
2015-02-19 22:39:36 +0800

    SortedMerge

---
+++ medium/talentbuddy/SortedMerge.java
@@ -0,0 +1,35 @@
+package talentbuddy;
+
+public class SortedMerge {
+	
+	public static void main(String[] args) {
+		Integer[] a = new Integer[] {2,3,7,8,8};
+		Integer[] b = new Integer[] {7,8,13};
+		merge_arrays(a, b);
+	}
+
+    public static void merge_arrays(Integer[] a, Integer[] b) {
+        int i = 0;
+        int j = 0;
+        
+        StringBuilder u = new StringBuilder();
+        while (i < a.length && j < b.length) {
+            if (a[i] < b[j]) {
+                u.append(a[i]).append(" ");
+                i++;
+            } else {
+                u.append(b[j]).append(" ");
+                j++;
+            }
+        }
+        
+        while (i < a.length) {
+            u.append(a[i++]).append(" ");
+        }
+        
+        while (j < b.length) {
+            u.append(b[j++]).append(" ");
+        }
+        System.out.println(u.toString().trim());
+    }
+}
