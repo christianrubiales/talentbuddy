5cc8ed1dcc0a62369cb7edc181adb6364eccea6b
Christian Rubiales <christianrubiales@yahoo.com>
2015-02-24 21:34:21 +0800

    CountTokens

---
+++ normal/CountTokens.java
@@ -0,0 +1,35 @@
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class CountTokens {
+
+	public static void main(String[] args) {
+		count_tokens("anmo", "anatomy");
+		count_tokens("elr", "hello there");
+	}
+
+    public static void count_tokens(String a, String t) {
+    	Set<Character> set = new HashSet<Character>();
+    	int c = 0;
+    	
+    	// add alphabet characters into a set
+    	for (int i = 0; i < a.length(); i++) {
+    		set.add(a.charAt(i));
+    	}
+    	
+    	// change from non-member to member are counted as tokens
+    	boolean prev = false;
+    	boolean curr = false;
+    	for (int i = 0; i < t.length(); i++) {
+    		curr = set.contains(t.charAt(i));
+    		
+    		if (!prev && curr) {
+    			c++;
+    		}
+    		prev = curr;
+    	}
+    	
+    	System.out.println(c);
+    }
+}
