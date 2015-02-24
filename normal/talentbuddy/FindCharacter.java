845244118e2514e2b74b554f01d7452da2b7fa39
Christian Rubiales <christianrubiales@yahoo.com>
2015-02-24 20:38:50 +0800

    FindCharacter

---
+++ normal/talentbuddy/FindCharacter.java
@@ -0,0 +1,24 @@
+package talentbuddy;
+
+public class FindCharacter {
+
+	public static void main(String[] args) {
+		find_chr("d", "abcdefabcdef");
+		find_chr("z", "abcdefabcdef");
+		find_chr("a", "abcdefabcdef");
+		find_chr("f", "abcdefabcdef");
+	}
+
+    public static void find_chr(String c, String t) {
+    	int index = -1;
+    	
+    	for (int i = 0; i < t.length(); i++) {
+    		if (t.charAt(i) == c.charAt(0)) {
+    			index = i + 1;
+    			break;
+    		}
+    	}
+    	
+    	System.out.println(index);
+    }
+}
