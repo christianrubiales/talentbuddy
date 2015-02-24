c37cc6d7cf5a528dd5b0b423c40fcc0e97482f47
Christian Rubiales <christianrubiales@yahoo.com>
2015-02-24 22:00:16 +0800

    FloatDivision

---
+++ normal/FloatDivision.java
@@ -0,0 +1,12 @@
+
+public class FloatDivision {
+
+	public static void main(String[] args) {
+		float_div(4, 8);
+		float_div(91, 35);
+	}
+
+    public static void float_div(Integer a, Integer b) {
+        System.out.println(((float) a) / ((float) b));
+    }
+}
