4b4ba8780b7b0ce3e2a54d0911ae99b8112c1757
Christian Rubiales <christianrubiales@yahoo.com>
2015-02-24 20:56:57 +0800

    CountSubstrings

---
+++ normal/CountSubstrings.java
@@ -0,0 +1,23 @@
+
+public class CountSubstrings {
+
+	public static void main(String[] args) {
+		count_substr("aa", "aaabaaaaa");
+		count_substr("abc", "ababcdabc");
+	}
+	
+    public static void count_substr(String s, String t) {
+    	int fromIndex = 0;
+    	int c = 0;
+    	
+    	while (fromIndex > -1) {
+    		fromIndex = t.indexOf(s, fromIndex);
+    		if (fromIndex > -1) {
+    			c++;
+    			fromIndex += s.length();
+    		}
+    	}
+    	System.out.println(c);
+    }
+
+}
