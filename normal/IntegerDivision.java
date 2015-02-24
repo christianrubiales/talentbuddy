d3aff3c4c920dd4b0fa97e3604d100ede33c52b1
Christian Rubiales <christianrubiales@yahoo.com>
2015-02-24 21:55:28 +0800

    IntegerDivision

---
+++ normal/IntegerDivision.java
@@ -0,0 +1,19 @@
+
+public class IntegerDivision {
+
+	public static void main(String[] args) {
+		int_div(7, 3);
+		int_div(Integer.MAX_VALUE, 1);
+	}
+
+    public static void int_div(Integer a, Integer b) {
+    	int quotient = 0;
+    	
+    	while (a > b) {
+    		a -= b;
+    		quotient++;
+    	}
+    	
+    	System.out.println(quotient);
+    }
+}
