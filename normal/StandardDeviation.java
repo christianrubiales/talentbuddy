0ecb73696cb3ae2feec0be7a3396d613ba332e77
Christian Rubiales <christianrubiales@yahoo.com>
2015-02-25 19:57:58 +0800

    StandardDeviation

---
+++ normal/StandardDeviation.java
@@ -0,0 +1,27 @@
+
+public class StandardDeviation {
+
+	public static void main(String[] args) {
+		compute_stddev(new Integer[]{35, 45, 78, 35, 23, 66, 75, 57});
+	}
+
+    public static void compute_stddev(Integer[] sample) {
+    	int mean = compute_mean(sample);
+    	
+    	int sum = 0;
+        for (Integer s : sample) {
+        	sum += (s - mean) * (s - mean);
+        }
+        
+        System.out.println((int) Math.sqrt(sum/sample.length));
+    }
+    
+    public static int compute_mean(Integer[] sample) {
+        int sum = 0;
+        
+        for (Integer s : sample) {
+            sum += s;
+        }
+        return sum / sample.length;
+    }
+}
