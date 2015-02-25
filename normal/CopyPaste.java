a06bf69e303fd795d0c40a2c0b3f8fe688109aba
Christian Rubiales <christianrubiales@yahoo.com>
2015-02-25 20:30:08 +0800

    CopyPaste

---
+++ normal/CopyPaste.java
@@ -0,0 +1,17 @@
+
+public class CopyPaste {
+
+	public static void main(String[] args) {
+		copy_string("abcdefghi", "xyz", 3); //abcxyzdefghi
+	}
+
+    public static void copy_string(String s1, String s2, Integer p) {
+    	for (int i = 0; i < p; i++) {
+    		System.out.print(s1.charAt(i));
+    	}
+    	System.out.print(s2);
+    	for (int i = p; i < s1.length(); i++) {
+    		System.out.print(s1.charAt(i));
+    	}
+    }
+}
