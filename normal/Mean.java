0caf1ace438de8313d9b9ffc9226667ddf61695a
Christian Rubiales <christianrubiales@yahoo.com>
2015-02-25 19:40:21 +0800

    Mean

---
+++ normal/Mean.java
@@ -0,0 +1,16 @@
+
+public class Mean {
+
+	public static void main(String[] args) {
+		compute_mean(new Integer[]{35, 45, 78, 35, 23, 66, 75, 57});
+	}
+
+    public static void compute_mean(Integer[] sample) {
+        int sum = 0;
+        
+        for (Integer s : sample) {
+            sum += s;
+        }
+        System.out.println(sum / sample.length);
+    }
+}
