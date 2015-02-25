0d874f072cae0500b1a1399e99c4d8aab79f99b8
Christian Rubiales <christianrubiales@yahoo.com>
2015-02-25 19:02:16 +0800

    Binary

---
+++ normal/Binary.java
@@ -0,0 +1,22 @@
+
+public class Binary {
+
+	public static void main(String[] args) {
+		convert_to_binary(156);
+		for (int i = 0; i < 10; i++) {
+			convert_to_binary(i);
+			System.out.println(Integer.toBinaryString(i));
+		}
+	}
+
+    public static void convert_to_binary(Integer n) {
+    	StringBuilder u = new StringBuilder();
+    	
+    	while (n > 0) {
+    		u.append(n % 2);
+    		n >>= 1;
+    	}
+    	
+    	System.out.println(u.reverse());
+    }
+}
