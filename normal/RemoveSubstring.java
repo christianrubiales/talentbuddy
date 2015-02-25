58d6016cb7c9009306c06a4966dbb75c431e9f54
Christian Rubiales <christianrubiales@yahoo.com>
2015-02-25 21:22:17 +0800

    RemoveSubstring

---
+++ normal/RemoveSubstring.java
@@ -0,0 +1,16 @@
+
+public class RemoveSubstring {
+
+	public static void main(String[] args) {
+		remove_substring("abcdefghi", 4, 3); // abcghi
+	}
+
+    public static void remove_substring(String s, Integer p, Integer n) {
+        for (int i = 0; i < p - 1; i++) {
+            System.out.print(s.charAt(i));
+        }
+        for (int i = p + n - 1; i < s.length(); i++) {
+            System.out.print(s.charAt(i));
+        }
+    }
+}
