74b8c2e234dc17d11c0e9f3288ddb9f514650d4e
Christian Rubiales <christianrubiales@yahoo.com>
2015-02-25 23:27:35 +0800

    CountDigits

---
+++ normal/CountDigits.java
@@ -0,0 +1,17 @@
+
+public class CountDigits {
+
+	public static void main(String[] args) {
+		count_digits("abc123"); // 3
+	}
+
+    public static void count_digits(String s) {
+    	int c = 0;
+    	for (int i = 0; i < s.length(); i++) {
+    		if  (Character.isDigit(s.charAt(i))) {
+    			c++;
+    		}
+    	}
+    	System.out.println(c);
+    }
+}
