1a62c365a1d1ce1a79ef7dd77d41793753342a6f
Christian Rubiales <christianrubiales@yahoo.com>
2015-02-25 21:11:08 +0800

    SelectSubstring

---
+++ normal/SelectSubstring.java
@@ -0,0 +1,13 @@
+
+public class SelectSubstring {
+
+	public static void main(String[] args) {
+		select_substring("abcdefghi", 2, 4);
+	}
+	
+    public static void select_substring(String s, Integer p1, Integer p2) {
+        for (int i = p1 - 1; i < p2; i++) {
+            System.out.print(s.charAt(i));
+        }
+    }
+}
