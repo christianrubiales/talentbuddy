e869c4ec861f841483cda363d5800b3316f5516c
Christian Rubiales <christianrubiales@yahoo.com>
2015-03-05 19:27:11 +0800

    PairProduct

---
+++ normal/PairProduct.java
@@ -0,0 +1,26 @@
+
+/**
+ * O(n)
+ */
+public class PairProduct {
+
+    public static void max_prod(Integer[] v) {
+        int max = 0;
+        for (int i = 0; i < v.length; i++) {
+            for (int j = 0; j < v.length; j++) {
+                if (i != j) {
+                    int t = v[i] * v[j];
+                    if (t % 3 == 0 && t > max) {
+                        max = t;
+                    }
+                }
+            }
+        }
+        System.out.println(max);
+    }
+    
+	public static void main(String[] args) {
+		max_prod(new Integer[]{6, 8, 8, 7, 2, 5});
+	}
+
+}
