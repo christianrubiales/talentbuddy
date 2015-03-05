3638e843b738b99a0ae0deba4200bc7e72a09df7
Christian Rubiales <christianrubiales@yahoo.com>
2015-03-05 19:53:27 +0800

    PairProduct

--- normal/PairProduct.java
+++ normal/PairProduct.java
@@ -1,31 +1,29 @@
 
 /**
- * Wrong: Get maximum number divisible by 3 and not divisible by 3, return the product.
+ * Iterate once, check if the number is the maximum divisible by 3, else if it is greater than other numbers.
+ * O(n)
  */
 public class PairProduct {
 
     public static void max_prod(Integer[] v) {
     	int maxDivisible = 0;
-    	int maxNotDivisible = 0;
+    	int maxOther = 0;
     	
-    	for (int i = 0; i < v.length; i++) {
-    		if (v[i] % 3 == 0) {
-    			if (v[i] > maxDivisible) {
-    				maxDivisible = v[i];
-    			}
-    		} else {
-    			if (v[i] > maxNotDivisible) {
-    				maxNotDivisible = v[i];
-    			}
+    	for (Integer i : v) {
+    		if (i % 3 == 0 && i > maxDivisible) {
+    			maxDivisible = i;
+    		} else if (i > maxOther) {
+    			maxOther = i;
     		}
     	}
     	
-    	System.out.println(maxDivisible * maxNotDivisible);
+    	System.out.println(maxDivisible * maxOther);
     }
     
 	public static void main(String[] args) {
 		max_prod(new Integer[]{6, 8, 8, 7, 2, 5}); // 48
 		max_prod(new Integer[]{6, 9, 9, 7, 2, 5}); // 81
+		max_prod(new Integer[]{8, 11, 10, 3, 9}); // 99
 	}
 
 }
