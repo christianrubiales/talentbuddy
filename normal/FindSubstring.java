d9fc285d0b31b9045c98c67c407af8d10a51c2f0
Christian Rubiales <christianrubiales@yahoo.com>
2015-04-01 23:00:57 +0800

    FindSubstring

--- normal/FindSubstring.java
+++ normal/FindSubstring.java
@@ -1,10 +1,29 @@
 
 /**
- * I want KMP or BM
+ * Brute force
  */
 public class FindSubstring {
 
+    public static void find_substr(String s, String t) {
+		for (int i = 0; i <= t.length() - s.length(); i++) {
+			for (int j = 0; j < s.length(); j++) {
+				if (t.charAt(i + j) == s.charAt(j)) {
+					if (j == s.length() - 1) {
+                        System.out.println(i + 1);
+						return;
+					}
+				} else {
+					break;
+				}
+			}
+		}
+		System.out.println(-1);
+		return;
+    }
+    
 	public static void main(String[] args) {
+		find_substr("cde", "abcdefabcdef");//3
+		find_substr("abd", "abcdefabcdef");//-1
 	}
 
 }
