073e209076ce98458d61a96f1dc3871fcbba1a51
Christian Rubiales <christianrubiales@yahoo.com>
2015-06-15 00:52:30 +0800

    MultiplyBy2

---
+++ medium/MultiplyBy2.java
@@ -0,0 +1,13 @@
+
+public class MultiplyBy2 {
+
+    public static void multiply_by_two(Integer n) {
+    	System.out.println(n << 1);
+    }
+	
+	public static void main(String[] args) {
+		multiply_by_two(4);//8
+		multiply_by_two(7);//14
+	}
+
+}
